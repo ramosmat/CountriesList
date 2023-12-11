0eb527787957a959c9000c88c12a478e1f30f333
Matheus <matheusramosbi.13@gmail.com>
2023-12-11 18:46:46 -0300

    add `async` functions

---
+++ app/page.tsx
@@ -0,0 +1,22 @@
+type Country = {
+  name: {
+    common: string;
+  };
+};
+
+async function getCountries(): Promise<Country[]> {
+  const response = await fetch("https://restcountries.com/v3.1/all");
+  return response.json();
+}
+
+export default async function Home() {
+  const countries = await getCountries();
+
+  return (
+    <section className="flex w-full container">
+      {countries.map((country) => (
+        <h1 key={country.name.common}>{country.name.common}</h1>
+      ))}
+    </section>
+  );
+}
